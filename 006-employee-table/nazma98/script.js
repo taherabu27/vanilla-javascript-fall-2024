e8b18e4375da90ec6b3fef52ae50b7d52327b468
nazma98 <nazmasarker1311@gmail.com>
2024-12-18 22:57:09 +0600

    Add employee project

---
+++ 006-employee-table/nazma98/script.js
@@ -0,0 +1,46 @@
+const employeeData = [
+  {
+    id: 1,
+    name: 'John Doe',
+    position: 'Software Engineer',
+    department: 'Engineering',
+  },
+  {
+    id: 2,
+    name: 'Jane Smith',
+    position: 'Product Manager',
+    department: 'Product',
+  },
+  { id: 3, name: 'Sam Johnson', position: 'Designer', department: 'Design' },
+  {
+    id: 4,
+    name: 'Chris Lee',
+    position: 'Data Scientist',
+    department: 'Data',
+  },
+];
+
+const employeeTableBodyElement = document.getElementById('employeeTableBody');
+
+const getEmployeeRow = (employee) => {
+  const columns = Object.keys(employee);
+  const tableRow = document.createElement('tr');
+  tableRow.className = 'hover:bg-gray-100';
+
+  columns.forEach(column => {
+    const employeeInfo = document.createElement('td');
+    employeeInfo.className = 'border px-4 py-2';
+    employeeInfo.innerText = employee[column];
+    tableRow.appendChild(employeeInfo);
+  });
+  return tableRow;
+}
+
+const showEmployeeTable = (employees) => {
+  employees.forEach(employee => {
+    const employeeRow = getEmployeeRow(employee);
+    employeeTableBodyElement.appendChild(employeeRow);
+  });
+}
+
+showEmployeeTable(employeeData);
