7a1d52576e4a95b262efe73b8961b422a33b6baa
sumiya-yasmin <sumiyayasmin665@gmail.com>
2024-12-20 21:25:07 -0300

    formatted the code

--- 006-employee-table/sumiya-yasmin/script.js
+++ 006-employee-table/sumiya-yasmin/script.js
@@ -1,40 +1,38 @@
-
 const employeeData = [
-    {
-      id: 1,
-      name: 'John Doe',
-      position: 'Software Engineer',
-      department: 'Engineering',
-    },
-    {
-      id: 2,
-      name: 'Jane Smith',
-      position: 'Product Manager',
-      department: 'Product',
-    },
-    { id: 3, name: 'Sam Johnson', position: 'Designer', department: 'Design' },
-    {
-      id: 4,
-      name: 'Chris Lee',
-      position: 'Data Scientist',
-      department: 'Data',
-    },
-  ];
+  {
+    id: 1,
+    name: 'John Doe',
+    position: 'Software Engineer',
+    department: 'Engineering',
+  },
+  {
+    id: 2,
+    name: 'Jane Smith',
+    position: 'Product Manager',
+    department: 'Product',
+  },
+  { id: 3, name: 'Sam Johnson', position: 'Designer', department: 'Design' },
+  {
+    id: 4,
+    name: 'Chris Lee',
+    position: 'Data Scientist',
+    department: 'Data',
+  },
+];
 const tableBody = document.getElementById('employeeTableBody');
 
-function renderTable(dataOfEmployee){
-dataOfEmployee.forEach((employee)=>{
-  const tableRow= document.createElement('tr');
-  tableRow.className = 'hover:bg-gray-100';
-  Object.keys(employee).forEach((value)=>{
-      const tableColumn= document.createElement('td');
-      tableColumn.innerText= employee[value];
+function renderTable(dataOfEmployee) {
+  dataOfEmployee.forEach((employee) => {
+    const tableRow = document.createElement('tr');
+    tableRow.className = 'hover:bg-gray-100';
+    Object.keys(employee).forEach((value) => {
+      const tableColumn = document.createElement('td');
+      tableColumn.innerText = employee[value];
       tableColumn.className = 'border px-4 py-2';
       tableRow.appendChild(tableColumn);
-  })
-  tableBody.appendChild(tableRow);
-})
+    });
+    tableBody.appendChild(tableRow);
+  });
 }
 
 renderTable(employeeData);
-
